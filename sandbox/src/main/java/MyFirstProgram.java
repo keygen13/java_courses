c656b3ee0f1b25c6b54317c4f7522d7a9d4e7b70
KeyGen <anja.seryogina@gmail.com>
2020-05-27 23:33:18 +0300

    Добавлено несколько методов

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -7,5 +7,27 @@ public class MyFirstProgram {
 		System.out.println(2 + 9);
 		System.out.println("2+2=" + 2 + 2);
 		System.out.println("2+2=" + (2 + 2));
+
+		hello("world");
+		hello("username");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
     }
+
+    public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+    	return len * len;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+	}
 }

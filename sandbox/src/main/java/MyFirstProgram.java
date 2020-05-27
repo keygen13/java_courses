cf45ccbe52f5364b1b778bec2d687a85536a4d7c
KeyGen <anja.seryogina@gmail.com>
2020-05-27 18:47:28 +0300

    Добавлены примеры арифметических операций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,11 @@
 public class MyFirstProgram {
 
-	public static void main (String[] args){
+    public static void main(String[] args) {
 
-		System.out.println("Hello!");
-	}
+        System.out.println("Hello!");
+
+		System.out.println(2 + 9);
+		System.out.println("2+2=" + 2 + 2);
+		System.out.println("2+2=" + (2 + 2));
+    }
 }

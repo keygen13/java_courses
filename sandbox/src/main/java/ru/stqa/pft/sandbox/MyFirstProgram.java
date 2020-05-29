c45af1347b08dca4d52948e1035bc786510a8c00
KeyGen <anja.seryogina@gmail.com>
2020-05-29 22:12:36 +0300

    Класс MyFirstProfram перемещён в пакет

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,14 +1,12 @@
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
 
         System.out.println("Hello!");
 
-		System.out.println(2 + 9);
-		System.out.println("2+2=" + 2 + 2);
-		System.out.println("2+2=" + (2 + 2));
-
-		hello("world");
+		hello("Anja");
 		hello("username");
 
 		double l = 5;

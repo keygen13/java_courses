1ed75fd5bdf9899d8edcc9c79c0b9d5243330ff8
KeyGen <anja.seryogina@gmail.com>
2020-06-01 22:43:20 +0300

    Added browser choice for Addressbook tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,10 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
     protected WebDriver wd;
 
     @BeforeMethod(alwaysRun = true)

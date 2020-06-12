c48c08288fe55597ceb06f90b1be64e14bf51bf0
KeyGen <anja.seryogina@gmail.com>
2020-06-12 10:23:53 +0300

    Added lists sorting to Contact tests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -13,7 +13,7 @@ public class ContactData {
     private String group;
 
     public ContactData(String firstname, String middlename, String lastname, String address, String homephone, String email, String group) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.firstname = firstname;
         this.middlename = middlename;
         this.lastname = lastname;
@@ -80,13 +80,12 @@ public class ContactData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ContactData that = (ContactData) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(firstname, that.firstname) &&
+        return Objects.equals(firstname, that.firstname) &&
                 Objects.equals(lastname, that.lastname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstname, lastname);
+        return Objects.hash(firstname, lastname);
     }
 }

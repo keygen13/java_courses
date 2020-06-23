295dc6fbc51ba41772e11de0158f2c574704f7b0
KeyGen <anja.seryogina@gmail.com>
2020-06-24 00:34:17 +0300

    Modified GroupModificationTests - added Sets comparison instead of List comparison.
    Лекция 5.5.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,35 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase {
 
-    @Test
-    public void testGroupModification() {
-        app.goTo().groupPage();
-        if(app.group().list().size() == 0) {
+    @BeforeMethod
+    public void  ensurePrecondition() {
+            app.goTo().groupPage();
+        if(app.group().all().size() == 0) {
             app.group().create(new GroupData().withName("test4"));
         }
+    }
 
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
+    @Test
+    public void testGroupModification() {
+        Set<GroupData> before = app.group().all();
+        GroupData groupToModify = before.iterator().next();
         GroupData group = new GroupData()
-                .withId(before.get(index).getId()).withName("ModificatedTest4").withHeader("ModificatedTest5").withFooter("ModificatedTest6");
-        app.group().modify(index, group);
-        List<GroupData> after = app.group().list();
+                .withId(groupToModify.getId()).withName("ModifiedTest4").withHeader("ModifiedTest5").withFooter("ModifiedTest6");
+        app.group().modify(group);
+        Set<GroupData> after = app.group().all();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(index);
+        before.remove(groupToModify);
         before.add(group);
-
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
 
         Assert.assertEquals(before, after);
     }

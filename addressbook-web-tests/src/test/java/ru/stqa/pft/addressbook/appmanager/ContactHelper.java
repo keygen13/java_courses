0f9f565e3ec4b1265238336b8df039028b1ffd87
KeyGen <anja.seryogina@gmail.com>
2020-06-29 17:49:34 +0300

    Added ContactAddressTests.
    Задание 11.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -92,6 +92,7 @@ public class ContactHelper extends HelperBase {
             String firstname = cells.get(2).getText();
             String allPhones = cells.get(5).getText();
             String allEmails = cells.get(4).getText();
+            String address = cells.get(3).getText();
 
             System.out.println(firstname + " " + lastname);
             int id = Integer.parseInt(w.findElement(By.tagName("input")).getAttribute("value"));
@@ -101,7 +102,8 @@ public class ContactHelper extends HelperBase {
                 .withFirstname(firstname)
                 .withLastname(lastname)
                 .withAllPhones(allPhones)
-                .withAllEmails(allEmails);
+                .withAllEmails(allEmails)
+                .withAddress(address);
 
             contacts.add(contactData);
         }
@@ -119,9 +121,10 @@ public class ContactHelper extends HelperBase {
         String email = wd.findElement(By.name("email")).getAttribute("value");
         String email2 = wd.findElement(By.name("email2")).getAttribute("value");
         String email3 = wd.findElement(By.name("email3")).getAttribute("value");
+        String address = wd.findElement(By.name("address")).getAttribute("value");
         wd.navigate().back();
         return new ContactData().withId(contact.getId()).withFirstname(firstname)
                 .withLastname(lastname).withHomePhone(home).withMobilePhone(mobile).withWorkPhone(work)
-                .withEmail(email).withEmail2(email2).withEmail3(email3);
+                .withEmail(email).withEmail2(email2).withEmail3(email3).withAddress(address);
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -1,5 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
-public class ContactAddressTests {
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+public class ContactAddressTests extends TestBase {
+
+    @Test
+    public void testContactAddress() {
+        app.goTo().goToHomePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
+    }
 }

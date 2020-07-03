50907ee831270442abac6ff886ab7d15ee34bbe0
KeyGen <anja.seryogina@gmail.com>
2020-07-03 03:43:29 +0300

    Added contact information extraction from DB.
    Lecture 7.3.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,35 +1,58 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "firstname")
     private  String firstname;
     @Expose
     private  String middlename;
     @Expose
+    @Column(name = "lastname")
     private  String lastname;
     @Expose
+    @Type(type = "text")
     private  String address;
     @Expose
+    @Column(name = "home")
+    @Type(type = "text")
     private  String homephone;
     @Expose
+    @Column(name = "work")
+    @Type(type = "text")
     private  String workphone;
     @Expose
+    @Column(name = "mobile")
+    @Type(type = "text")
     private  String mobilephone;
     @Expose
+    @Transient
     private  String email;
+    @Transient
     private  String email2;
+    @Transient
     private  String email3;
     @Expose
+    @Transient
     private  String group;
+    @Transient
     private  String allphones;
+    @Transient
     private  String allemails;
-    private File photo;
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
 
     /*public void setId(int id) {
@@ -82,7 +105,7 @@ public class ContactData {
 
     public String getGroup() { return group; }
 
-    public File getPhoto() { return photo; }
+    public File getPhoto() { return new File(photo); }
 
     public ContactData withId(int id) {
         this.id = id;
@@ -155,7 +178,7 @@ public class ContactData {
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 

e16c2c8427911db32bcb35b9a735a0c58489c5c5
KeyGen <anja.seryogina@gmail.com>
2020-07-03 04:57:16 +0300

    Modified GroupDeletionTests in order to use DbHelper.
    Task 15.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -13,20 +13,21 @@ public class GroupDeletionTests extends TestBase {
 
   @BeforeMethod
   public void  ensurePrecondition() {
-    app.goTo().groupPage();
-    if(app.group().all().size() == 0) {
-      app.group().create(new GroupData().withName("test1"));
+      if(app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test4"));
     }
   }
 
   @Test
   public void testGroupDeletion() throws Exception {
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData groupForDeletion = before.iterator().next();
+    app.goTo().groupPage();
     app.group().delete(groupForDeletion);
 
     assertThat(app.group().count(), equalTo(before.size()-1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
 
     assertThat(after, equalTo(before.without(groupForDeletion)));
     }

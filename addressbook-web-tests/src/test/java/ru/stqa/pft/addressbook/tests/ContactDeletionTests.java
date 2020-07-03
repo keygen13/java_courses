2b459b2bdb8826798ac392d3e7bb22169ae84c9a
KeyGen <anja.seryogina@gmail.com>
2020-07-04 00:44:07 +0300

    Edited precondition in ContactDeletionTests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -5,7 +5,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.File;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -17,10 +19,13 @@ public class ContactDeletionTests extends TestBase {
     @BeforeMethod
     public void ensurePrecondition() {
         if (app.db().contacts().size() == 0) {
+            Groups groups = app.db().groups();
+            File photo = new File("src/test/resources/goldie.jpg");
             app.goTo().goToHomePage();
-            app.getContactHelper().createContact(new ContactData().withFirstname("Goldie").withMiddlename("Jeanne")
-                    .withLastname("Hawn").withAddress("Hollywood").withHomePhone("555555").withMobilePhone("+3421")
-                    .withWorkPhone("223 444").withEmail("goldie@12.ru").withGroup("test4"), true);
+            ContactData newContact = new ContactData().withFirstname("Goldie").withMiddlename("Jeanne").withLastname("Hawn")
+                    .withAddress("Hollywood").withHomePhone("555555").withMobilePhone("+3421").withWorkPhone("223 444")
+                    .withEmail("goldie@12.ru").inGroup(groups.iterator().next()).withPhoto(photo);
+            app.getContactHelper().createContact(newContact, true);
         }
     }
 

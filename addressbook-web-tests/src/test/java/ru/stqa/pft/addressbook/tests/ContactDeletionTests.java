c9da8315faa54eed04d93238bbae9cad40a0cd94
KeyGen <anja.seryogina@gmail.com>
2020-07-03 06:12:02 +0300

    Bugged ContactDeletionTests.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -13,7 +15,8 @@ public class ContactDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePrecondition() {
-        if (! app.getContactHelper().isThereAContact()) {
+        if (app.db().contacts().size() == 0) {
+            app.goTo().goToHomePage();
             app.getContactHelper().createContact(new ContactData().withFirstname("Goldie").withMiddlename("Jeanne")
                     .withLastname("Hawn").withAddress("Hollywood").withHomePhone("555555").withMobilePhone("+3421")
                     .withWorkPhone("223 444").withEmail("goldie@12.ru").withGroup("test4"), true);
@@ -22,13 +25,13 @@ public class ContactDeletionTests extends TestBase {
 
     @Test(enabled = true)
     public void testContactDeletion() {
-        Contacts before = app.getContactHelper().all();
+        Contacts before = app.db().contacts();
 
         ContactData contactToDelete = before.iterator().next();
-        app.getContactHelper().deleteContact(contactToDelete);
         app.goTo().goToHomePage();
+        app.getContactHelper().deleteContact(contactToDelete);
 
-        Contacts after = app.getContactHelper().all();
+        Contacts after = app.db().contacts();
 
         assertEquals(after.size(), before.size() - 1);
         assertThat(after, equalTo(before.without(contactToDelete)));
